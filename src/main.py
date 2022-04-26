088f7344be2dd0a2118ab440bf537b87c02fefab
danielseal <dan.seal.06@gmail.com>
2022-04-26 17:10:54 +0100

    Adding in repo code

---
+++ src/main.py
@@ -0,0 +1,7 @@
+
+from modules.app import run_app
+
+
+if __name__ == '__main__':
+    
+    run_app()
---
+++ src/modules/app.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+
+def get_data_from_cloud_database(key):
+    """Connect to an api's database and retreive data."""
+    if key is None:
+        raise ConnectionError("Cannot connect to database.")
+
+
+def do_some_calculation(data: pd.DataFrame, column: str):
+    """Return the inputted data with the specified column's values multiplied by -1 (signs flipped).
+
+    Args:
+        data (pd.DataFrame): Input data.
+        column (str): column in data to be multiplied by -1.
+
+    Returns:
+        pd.DataFrame: DataFrame with a reversed sign column.
+    """
+    # flip the sign of an entire numerical column of data.
+    data[column] = -1*data[column]
+
+    return data
+
+
+def write_processed_data_back_to_cloud_database():
+    """Connect to an api's database and retreive data."""
+    raise ConnectionError("Cannot write data back to database.")
+
+
+def run_app():
+    """Run app."""
+
+    df = get_data_from_cloud_database(key=None)
+    
+    df = do_some_calculation(data=df, column='A')
+    df = do_some_calculation(data=df, column='C')
+    
+    write_processed_data_back_to_cloud_database()
+
+    return df
